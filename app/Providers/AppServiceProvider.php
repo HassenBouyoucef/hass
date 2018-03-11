ad8f2af4fa41f35150a0fa288410d1f8edac65e3
HassenBouyoucef <bouyoucefhassen93@gmail.com>
2018-03-11 13:06:01 +0100

    yyyyy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,12 +12,9 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		if(env('REDIRECT_HTTPS')) {
-            $url->formatScheme('https');
-        }
 		//
 		App::bind('path.public', function() {
-		    return base_path().'/public';
+		    return base_path().'/public_html';
 		});
 	}
 
@@ -32,9 +29,6 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		if(env('REDIRECT_HTTPS')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
 		$this->app->bind(
 			'Illuminate\Contracts\Auth\Registrar',
 			'App\Services\Registrar'
