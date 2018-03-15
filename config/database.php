08255d51e8b1e215384aaa11ea4196b75097bd87
HassenBouyoucef <bouyoucefhassen93@gmail.com>
2018-03-15 17:59:14 +0100

    jjjj

--- config/database.php
+++ config/database.php
@@ -1,11 +1,5 @@
 <?php
 
-$url = parse_url(getenv('DATABASE_URL'));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"],1);
-
 return [
 
 	/*
@@ -32,7 +26,7 @@ return [
 	|
 	*/
 
-	'default' => 'mysql_production',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -64,18 +58,6 @@ return [
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-			'strict'    => false,
-		],
-
-		'mysql_production' => [
-			'driver'    => 'mysql',
-			'host'      => $host,
-			'database'  => $database,
-			'username'  => $username,
-			'password'  => $password,
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
