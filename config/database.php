f8a7a5322fbeadca96b0be555c179a92e1be1875
HassenBouyoucef <bouyoucefhassen93@gmail.com>
2018-03-17 17:39:56 +0100

    ttt

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
 	/*
@@ -26,7 +33,7 @@ return [
 	|
 	*/
 
-	'default' => 'mysql',
+	'default' => 'mysql_prod',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -51,6 +58,17 @@ return [
 			'database' => storage_path().'/database.sqlite',
 			'prefix'   => '',
 		],
+
+		'mysql_prod' => array(
+        'driver'    => 'mysql',
+        'host'      => $host,
+        'database'  => $database,
+        'username'  => $username,
+        'password'  => $password,
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ),
 
 		'mysql' => [
 			'driver'    => 'mysql',
